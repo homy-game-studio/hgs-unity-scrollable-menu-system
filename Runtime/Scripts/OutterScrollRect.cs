a029a9467498d64939b72c653a0c0d3f85d0ada5
Douglas da Silva <douglasdasilva448@gmail.com>
2021-06-18 00:41:48 -0300

    feat: primeira versão

---
+++ Runtime/Scripts/OutterScrollRect.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+namespace HGS.ScrollableMenuSystem
+{
+    public class OutterScrollRect : ScrollRect
+    {
+        public Action<PointerEventData> onBeginDrag = null;
+        public Action<PointerEventData> onDrag = null;
+        public Action<PointerEventData> onEndDrag = null;
+
+        public override void OnBeginDrag(PointerEventData eventData)
+        {
+            base.OnBeginDrag(eventData);
+            onBeginDrag?.Invoke(eventData);
+        }
+
+        public override void OnEndDrag(PointerEventData eventData)
+        {
+            base.OnEndDrag(eventData);
+            onEndDrag?.Invoke(eventData);
+        }
+
+        public override void OnDrag(PointerEventData eventData)
+        {
+            base.OnDrag(eventData);
+            onDrag?.Invoke(eventData);
+        }
+    }
+}
